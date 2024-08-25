29929341ed59a97b9bf37d6a921874ed13f1aa40
AbbyGoose <157957828+AbbyGoose@users.noreply.github.com>
2024-08-25 09:26:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def global_w():
+    return render_template('index.html')
+
+@app.route('/reason')
+def reason():
+    return render_template('reason.html')
+
+@app.route('/next')
+def next():
+    return render_template('next.html')
+
+@app.route('/choice')
+def choice():
+    return render_template('choice.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
